a5175199f7e0aae8294809a945ec311dbede7775
Aaron Wells <alwells239@gmail.com>
2023-06-26 10:17:58 -0400

    Initial Commit

---
+++ Little Lemon IOS/Little_Lemon_IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Little_Lemon_IOSApp.swift
+//  Little Lemon IOS
+//
+//  Created by Aaron Wells on 6/26/23.
+//
+
+import SwiftUI
+
+@main
+struct Little_Lemon_IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
